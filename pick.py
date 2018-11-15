0a3daa8fac7cc1cd017afa23f204c200dbe6201c
falownik <36796295+falownik@users.noreply.github.com>
2018-11-16 00:45:12 +0100

    Update pick.py

--- pick.py
+++ pick.py
@@ -11,28 +11,29 @@ class Robot:
   right_engine = LargeMotor('outB')
   left_engine = LargeMotor('outA')
   
-  
-  
-  def follow_the_line():
+    #parametry PD
     Kp = 4.0
     Kd = 1.0
-    predkosc_bazowa = 60.0
-    srodek_l = (white_left + black_right) // 2
-    srodek_r = (white_right + black_right) // 2
+    base_speed = 60.0
+    mid_l = (white_left + black_right) // 2
+    mid_r = (white_right + black_right) // 2
     blad = 0
-    poprzedni_blad = 0
-    blad_l = 0
-    blad_r = 0
-    blad_prop = 0.0
-    blad_deri = 0.0
+    last_error = 0
+    error_l = 0
+    error_r = 0
+    error_P = 0.0
+    error_D = 0.0
+    
+    
+  def follow_the_line():
 
     while not touch_sensor.is_pressed:
-      blad_l = light_left.reflected_light_intensity - srodek_l
-      blad_r = light_right.reflected_light_intensity - srodek_r
-      blad = blad_l - blad_r
-      blad_prop = Kp * float(blad)
-      blad_deri = Kd * (blad - poprzedni_blad)
-      left_engine.run_timed(time_sp = 100, speed_sp = -predkosc_bazowa - blad_prop + blad_deri, stop_action = "coast")
-      right_engine.run_timed(time_sp = 100, speed_sp = -predkosc_bazowa + blad_prop - blad_deri, stop_action = "coast")
-      poprzedni_blad = blad
+      error_l = light_left.reflected_light_intensity - mid_l
+      error_r = light_right.reflected_light_intensity - mid_r
+      error = error_l - error_r
+      error_P = Kp * float(error)
+      error_D = Kd * (error - last_error)
+      left_engine.run_timed(time_sp = 100, speed_sp = -base_speed - error_P + error_D, stop_action = "coast")
+      right_engine.run_timed(time_sp = 100, speed_sp = -base_speed + error_P - error_D, stop_action = "coast")
+      last_error = error
   
